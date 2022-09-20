9f0987a86567112510bed92846b3463020ffa2ef
João Ramos <jpramos.comp@gmail.com>
2022-09-20 14:19:35 -0300

    created intro section

---
+++ src/components/IntroTrajar.vue
@@ -0,0 +1,108 @@
+<template>
+    <section class="intro">
+        <div class="intro-container">
+           
+                <h1> ALFAIATE</h1>
+          
+            <h2>COMPRE HOJE, <span>PAGUE EM ATÉ 3X COM 12% DE DESCONTO</span>  E TENHA
+                10 MESES PARA CONFECCIONAR!</h2>
+                
+                <a class="intro-btn" href="#">FAÇA UM ORÇAMENTO</a>
+           
+        </div>             
+    </section>
+</template>
+
+<script>
+
+export default ({
+    name:'IntroTrajar'
+})
+</script>
+<style>
+
+.intro {
+    background: url('@/assets/banner.png') no-repeat center;
+    background-size: cover;      
+    color: #fff;
+}
+
+.intro-container{
+    max-width: 1366px;
+    height: 630px;
+    margin: 0 auto; 
+    display: flex;
+    flex-direction: column;
+        
+    }
+
+.intro h1{
+    width: 404.18px;
+    height: 119.58px;  
+    font-family: 'Playfair Display';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 5.5em;
+    line-height: 120px;
+    color: rgba(2, 75, 104, 0.68);
+    margin-top: 150px;
+        
+}
+.intro h1::before{
+    content:'Elegante é ter um';
+    display: block;
+    font-family: 'Playfair Display';
+    font-style: italic;
+    font-weight: 400;
+    font-size: 32.5627px;
+    line-height: 43px;
+    color: #BE9667;
+}
+
+.intro h1::after{
+    content:'para chamar de seu.';
+    display: block;  
+    font-family: 'Playfair Display';
+    font-style: italic;
+    font-weight: 400;
+    font-size: 32.5627px;
+    line-height: 43px;
+    color: #BE9667; 
+    padding-left: 135px;   
+}
+
+.intro h2{
+    margin-top: 150px;    
+    max-width: 438px;
+    max-height: 93px;   
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 131%;    
+    text-transform: uppercase;
+    color: #949494;     
+}
+
+.intro span{
+    color: #fff; 
+}
+.intro-btn{     
+    font-family: 'Lato';
+    font-style: normal;
+	text-decoration: none;
+	text-align: center;
+	font-size: 1.125em;	
+	text-transform: uppercase;
+	display: block;
+	width: 300px;
+	padding: 15px 0;	
+    background: #014561;
+    border-radius: 5px;
+    color: #E5E5E5;
+    margin-top: 20px;
+}
+.intro-btn a{
+    color: #E5E5E5;
+}
+</style>
---
+++ src/components/ReaderTrajar.vue
@@ -0,0 +1,80 @@
+<template>
+    <header class="menu">
+        <div class="menu-container">
+            <a class="menu-logo" href="#"><img src="@/assets/logo.png" alt="logo"></a>
+           
+            <nav class="menu-nav">
+                <ul>
+                    <li> <a href="#">Empresa</a></li>
+                    <li> <a href="#">Camisaria</a></li>
+                    <li> <a href="#">Alfaiataria</a></li>
+                    <li> <a href="#">Depoimentos</a></li>
+                    <li> <a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name: 'ReaderTrajar'
+    }
+</script>
+<style>
+   /* Reset */
+body,
+h1,
+h2,
+h3,
+p,
+ul {
+    padding: 0px;
+    margin: 0px;
+}
+
+body {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: #252525;
+}
+.menu{
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;  
+    
+}
+.menu-container{
+    max-width: 1366px;
+    max-height: 111px;
+    margin: 0 auto;
+    background: linear-gradient(180deg, #000000 29.73%, rgba(0, 0, 0, 0) 100%);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-top: 20px;
+    
+}
+
+.menu-nav li{
+    display: inline-block;
+}
+.menu-nav a{
+    display: block;
+    padding: 10px;
+    text-decoration: none;
+    color: #fff;
+    font-size: 1.125em;
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 19px;
+    text-transform: uppercase;
+    color: #949494;
+
+}
+
+</style>>
+ 
+
